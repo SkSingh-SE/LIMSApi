e6428f906f291ae0c522f1e9c403e48f44df5a98
SkSingh-SE <sudhanshu.s@tripearltech.com>
2026-07-17 12:34:24 +0530

    Added migration to updated database changes of designation field udpate

---
+++ LIMSApi/Migrations/20260717065036_UpdateDesignationFieldLength.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LIMSApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDesignationFieldLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RolesAndResponsibilities",
+                table: "DesignationMasters",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalityTraits",
+                table: "DesignationMasters",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "DesignationMasters",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RolesAndResponsibilities",
+                table: "DesignationMasters",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalityTraits",
+                table: "DesignationMasters",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "DesignationMasters",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000,
+                oldNullable: true);
+        }
+    }
+}
